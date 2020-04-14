e69f81c6c318b7eac1abb69a5136106ed983e0fc
Philipp Nieting <philipp@nieting.de>
2020-04-14 19:40:32 +0200

    Added JUnit EndpointCondition
    * Executes tests dependent on the TestConfig

---
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/junitExtensions/EndpointCondition.java
@@ -0,0 +1,47 @@
+package de.rub.nds.tlstest.framework.junitExtensions;
+
+import de.rub.nds.tlstest.framework.TestContext;
+import de.rub.nds.tlstest.framework.annotations.ClientTest;
+import de.rub.nds.tlstest.framework.annotations.ServerTest;
+import de.rub.nds.tlstest.framework.config.TestConfig;
+import de.rub.nds.tlstest.framework.constants.TestEndpointType;
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.lang.reflect.Method;
+
+public class EndpointCondition implements ExecutionCondition {
+
+    private TestEndpointType endpointOfMethod(ExtensionContext context) {
+        Method testMethod = context.getRequiredTestMethod();
+        Class<?> testClass = context.getRequiredTestClass();
+
+        if (testMethod.isAnnotationPresent(ClientTest.class)) {
+            return TestEndpointType.CLIENT;
+        } else if (testMethod.isAnnotationPresent(ServerTest.class)) {
+            return TestEndpointType.SERVER;
+        } else if (testClass.isAnnotationPresent(ClientTest.class)) {
+            return TestEndpointType.CLIENT;
+        } else if (testClass.isAnnotationPresent(ServerTest.class)) {
+            return TestEndpointType.SERVER;
+        } else {
+            return TestEndpointType.BOTH;
+        }
+    }
+
+    @Override
+    public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
+        if (!context.getTestMethod().isPresent()) {
+            return ConditionEvaluationResult.enabled("Class annotations are not relevant.");
+        }
+
+        TestConfig config = TestContext.getInstance().getConfig();
+
+        if (endpointOfMethod(context) == config.getTestEndpointMode() || endpointOfMethod(context) == TestEndpointType.BOTH) {
+            return ConditionEvaluationResult.enabled("TestEndpointMode matches");
+        }
+
+        return ConditionEvaluationResult.disabled("TestEndpointMode doesn't match, skipping test.");
+    }
+}

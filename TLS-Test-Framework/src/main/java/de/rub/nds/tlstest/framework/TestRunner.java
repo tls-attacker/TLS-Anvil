da4ca56b40f0a7e731fd83fcbe1a562af6aee706
Philipp Nieting <philipp@nieting.de>
2020-04-14 19:35:15 +0200

    Added tag support for TestRunner

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/TestRunner.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/TestRunner.java
@@ -1,10 +1,11 @@
 package de.rub.nds.tlstest.framework;
 
-import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlstest.framework.config.TestConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -12,24 +13,32 @@ import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 import java.io.PrintWriter;
 
-import static org.junit.platform.engine.discovery.DiscoverySelectors.*;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
 public class TestRunner {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private Config baseConfig;
+    private TestConfig testConfig;
 
-    public TestRunner(Config baseConfig) {
-        this.baseConfig = baseConfig;
+    public TestRunner(TestConfig testConfig) {
+        this.testConfig = testConfig;
     }
 
     public void runTests(Class<?> mainClass) {
         String packageName = mainClass.getPackage().getName();
-        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+        LauncherDiscoveryRequestBuilder builder = LauncherDiscoveryRequestBuilder.request()
             .selectors(
                     selectPackage(packageName)
-            )
-            .build();
+            );
+
+
+        if (testConfig.getTags().size() > 0) {
+            builder.filters(
+                    TagFilter.includeTags(testConfig.getTags())
+            );
+        }
+
+        LauncherDiscoveryRequest request = builder.build();
 
         Launcher launcher = LauncherFactory.create();
 

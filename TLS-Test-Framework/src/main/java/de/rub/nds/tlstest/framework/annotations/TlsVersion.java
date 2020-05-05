1eea1bfad44367b6df4b7a11a2ebc31c0a8b1c4b
Philipp Nieting <philipp@nieting.de>
2020-05-05 18:48:24 +0200

    Fixed TlsVersion annotation inheritance

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/annotations/TlsVersion.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/annotations/TlsVersion.java
@@ -2,11 +2,9 @@ package de.rub.nds.tlstest.framework.annotations;
 
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface TlsVersion {
---
+++ TLS-Test-Framework/src/test/java/de/rub/nds/tlstest/framework/annotations/tlsVersion/TlsVersionAnnotationSubclass.java
@@ -0,0 +1,55 @@
+package de.rub.nds.tlstest.framework.annotations.tlsVersion;
+
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import de.rub.nds.tlsscanner.report.SiteReport;
+import de.rub.nds.tlstest.framework.TestContext;
+import de.rub.nds.tlstest.framework.annotations.TlsTest;
+import de.rub.nds.tlstest.framework.annotations.TlsVersion;
+import de.rub.nds.tlstest.framework.junitExtensions.TlsVersionCondition;
+import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
+import de.rub.nds.tlstest.framework.utils.ConditionTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.util.ArrayList;
+
+@TlsVersion(supported = ProtocolVersion.TLS12)
+class Tls12SuperClass {}
+
+public class TlsVersionAnnotationSubclass extends Tls12SuperClass  {
+
+    @RegisterExtension
+    static ConditionTest ext = new ConditionTest(TlsVersionCondition.class);
+
+    @BeforeAll
+    static void setup() {
+        TestContext testContext = new TestContext();
+        SiteReport report = new SiteReport("", new ArrayList<>());
+
+        report.setVersions(new ArrayList<ProtocolVersion>() {
+            {
+                add(ProtocolVersion.TLS12);
+                add(ProtocolVersion.SSL3);
+            }
+        });
+
+        testContext.getConfig().setSiteReport(report);
+    }
+
+
+    @TlsTest
+    @TlsVersion(supported = ProtocolVersion.TLS12)
+    public void execute_supported() { }
+
+    @TlsTest
+    public void execute_inheritedClassAnnotation() { }
+
+    @TlsTest
+    @TlsVersion(supported = ProtocolVersion.SSL3)
+    public void execute_supported_overwrittenClassAnnotation() { }
+
+    @TlsTest
+    @TlsVersion(supported = ProtocolVersion.TLS13)
+    public void not_execute_unsupported() { }
+
+}

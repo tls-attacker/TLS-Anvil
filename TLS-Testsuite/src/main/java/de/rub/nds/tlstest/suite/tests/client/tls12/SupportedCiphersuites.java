f2954cd046488e536c530699228c7a1639a8daf4
Philipp Nieting <philipp@nieting.de>
2020-07-08 13:15:40 +0200

    Added new client test

---
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls12/SupportedCiphersuites.java
@@ -0,0 +1,34 @@
+package de.rub.nds.tlstest.suite.tests.client.tls12;
+
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
+import de.rub.nds.tlstest.framework.annotations.ClientTest;
+import de.rub.nds.tlstest.framework.annotations.TlsTest;
+import de.rub.nds.tlstest.framework.constants.SeverityLevel;
+import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+@ClientTest
+public class SupportedCiphersuites extends Tls12Test {
+
+    @TlsTest(description = "Client exploration detected moresupported ciphersuites than " +
+            "advertised by the client in the ClientHello message.", securitySeverity = SeverityLevel.MEDIUM)
+    public void supportsMoreCiphersuitesThanAdvertised() {
+        ClientHelloMessage clientHello = context.getReceivedClientHelloMessage();
+
+        List<CipherSuite> advertised = CipherSuite.getCiphersuites(clientHello.getCipherSuites().getValue());
+        List<CipherSuite> supported = new ArrayList<>(context.getConfig().getSiteReport().getCipherSuites());
+
+        advertised.forEach(supported::remove);
+
+        assertEquals("Client supports more ciphersuites than advertised. " +
+                        supported.parallelStream().map(Enum::name).collect(Collectors.joining(",")),
+                0,
+                supported.size());
+    }
+}

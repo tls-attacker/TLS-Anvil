261aa30fcb2a2180273ba910c3cfd5197d8eae05
Philipp Nieting <philipp@nieting.de>
2020-08-03 14:35:45 +0200

    Bugfix

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls12/rfc6066/ServerNameIndication.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls12/rfc6066/ServerNameIndication.java
@@ -3,9 +3,9 @@ package de.rub.nds.tlstest.suite.tests.client.tls12.rfc6066;
 import de.rub.nds.tlsattacker.core.constants.NameType;
 import de.rub.nds.tlsattacker.core.protocol.message.extension.ServerNameIndicationExtensionMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.extension.sni.ServerNamePair;
+import de.rub.nds.tlstest.framework.annotations.ClientTest;
 import de.rub.nds.tlstest.framework.annotations.MethodCondition;
 import de.rub.nds.tlstest.framework.annotations.RFC;
-import de.rub.nds.tlstest.framework.annotations.ServerTest;
 import de.rub.nds.tlstest.framework.annotations.TlsTest;
 import de.rub.nds.tlstest.framework.execution.WorkflowRunner;
 import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.junit.Assert.assertFalse;
 
 @RFC(number = 6066, section = "3. Server Name Indication")
-@ServerTest
+@ClientTest
 public class ServerNameIndication extends Tls12Test {
 
     public ConditionEvaluationResult sniActive() {

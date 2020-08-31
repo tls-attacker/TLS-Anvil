f1d1c83b960c27084823466f306e1100b4ef51a9
Philipp Nieting <philipp@nieting.de>
2020-08-31 17:41:22 +0200

    Added tags to test classes

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/testClasses/Tls12Test.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/testClasses/Tls12Test.java
@@ -5,9 +5,11 @@ import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlstest.framework.annotations.KeyExchange;
 import de.rub.nds.tlstest.framework.annotations.TlsVersion;
 import de.rub.nds.tlstest.framework.constants.KeyExchangeType;
+import org.junit.jupiter.api.Tag;
 
 @TlsVersion(supported = ProtocolVersion.TLS12)
 @KeyExchange(supported = KeyExchangeType.ALL12)
+@Tag("tls12")
 public class Tls12Test extends TlsBaseTest {
     @Override
     public Config getConfig() {
--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/testClasses/Tls13Test.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/testClasses/Tls13Test.java
@@ -6,9 +6,11 @@ import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlstest.framework.annotations.KeyExchange;
 import de.rub.nds.tlstest.framework.annotations.TlsVersion;
 import de.rub.nds.tlstest.framework.constants.KeyExchangeType;
+import org.junit.jupiter.api.Tag;
 
 @TlsVersion(supported = ProtocolVersion.TLS13)
 @KeyExchange(supported = KeyExchangeType.ALL13)
+@Tag("tls13")
 public class Tls13Test extends TlsBaseTest {
     @Override
     public Config getConfig() {

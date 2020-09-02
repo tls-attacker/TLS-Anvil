ebe598559adb4228b0ce6d2afe715eda38b09db9
Philipp Nieting <philipp@nieting.de>
2020-09-02 13:35:26 +0200

    Added test for CVE-2020-24613

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls13/stateMachine/Finished.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls13/stateMachine/Finished.java
@@ -1,4 +1,34 @@
-package de.rub.nds.tlstest.suite.tests.server.tls13.stateMachine;
+package de.rub.nds.tlstest.suite.tests.client.tls13.stateMachine;
 
-public class Finished {
+import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
+import de.rub.nds.tlsattacker.core.protocol.message.AlertMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.FinishedMessage;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
+import de.rub.nds.tlsattacker.core.workflow.action.ReceiveAction;
+import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
+import de.rub.nds.tlstest.framework.Validator;
+import de.rub.nds.tlstest.framework.annotations.ClientTest;
+import de.rub.nds.tlstest.framework.annotations.TlsTest;
+import de.rub.nds.tlstest.framework.constants.SeverityLevel;
+import de.rub.nds.tlstest.framework.execution.WorkflowRunner;
+import de.rub.nds.tlstest.framework.testClasses.Tls13Test;
+
+@ClientTest
+public class Finished extends Tls13Test {
+
+    @TlsTest(description = "CVE-2020-24613, Send Finished without Certificate", securitySeverity = SeverityLevel.CRITICAL)
+    public void sendFinishedWithoutCert(WorkflowRunner runner) {
+        runner.replaceSelectedCiphersuite = true;
+
+        Config c = this.getConfig();
+        WorkflowTrace workflowTrace = runner.generateWorkflowTraceUntilSendingMessage(WorkflowTraceType.HELLO, HandshakeMessageType.CERTIFICATE);
+        workflowTrace.addTlsActions(
+                new SendAction(new FinishedMessage()),
+                new ReceiveAction(new AlertMessage())
+        );
+
+        runner.execute(workflowTrace, c).validateFinal(Validator::receivedFatalAlert);
+    }
 }

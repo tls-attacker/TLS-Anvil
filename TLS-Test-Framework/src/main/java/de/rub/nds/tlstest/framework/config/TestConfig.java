de31c97b07d96ff937510214b86f41998194f38d
Philipp Nieting <philipp@nieting.de>
2020-09-16 17:13:59 +0200

    Added README and License

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/config/TestConfig.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/config/TestConfig.java
@@ -1,3 +1,12 @@
+/**
+ * TLS-Test-Framework - A framework for modeling TLS tests
+ *
+ * Copyright 2020 Ruhr University Bochum and
+ * TÜV Informationstechnik GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 package de.rub.nds.tlstest.framework.config;
 
 import com.beust.jcommander.JCommander;

3a875984511705dfa2f9a77bfd3701aa54594ba2
mmaehren <32199075+mmaehren@users.noreply.github.com>
2021-01-07 14:01:05 +0100

    Always offer the same ALPN protocols as ExtensionProbe of Scanner

--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/config/TestConfig.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/config/TestConfig.java
@@ -299,6 +299,9 @@ public class TestConfig extends TLSDelegateConfig {
         config.setStealthMode(true);
         config.setRetryFailedClientTcpSocketInitialization(true);
         config.setReceiveFinalTcpSocketStateWithTimeout(true);
+        
+        config.setDefaultProposedAlpnProtocols("http/1.1", "spdy/1", "spdy/2", "spdy/3", "stun.turn",
+                "stun.nat-discovery", "h2", "h2c", "webrtc", "c-webrtc", "ftp", "imap", "pop3", "managesieve");
 
         cachedConfig = config;
         return config;

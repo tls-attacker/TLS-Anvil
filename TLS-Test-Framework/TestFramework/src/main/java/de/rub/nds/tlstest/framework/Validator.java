4ea083fcb23214e36cb7843637aa7e5b5e0c096d
mmaehren <32199075+mmaehren@users.noreply.github.com>
2021-01-21 09:20:00 +0100

    Improved validation of alerts that have been encrypted too early

--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/Validator.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/Validator.java
@@ -9,11 +9,25 @@
  */
 package de.rub.nds.tlstest.framework;
 
+import de.rub.nds.modifiablevariable.util.Modifiable;
 import de.rub.nds.tlsattacker.core.constants.AlertDescription;
 import de.rub.nds.tlsattacker.core.constants.AlertLevel;
 import de.rub.nds.tlsattacker.core.constants.ProtocolMessageType;
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.core.constants.Tls13KeySetType;
+import de.rub.nds.tlsattacker.core.protocol.handler.AlertHandler;
 import de.rub.nds.tlsattacker.core.protocol.message.AlertMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ApplicationMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ChangeCipherSpecMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.ProtocolMessage;
+import de.rub.nds.tlsattacker.core.record.AbstractRecord;
+import de.rub.nds.tlsattacker.core.record.Record;
+import de.rub.nds.tlsattacker.core.record.cipher.RecordCipher;
+import de.rub.nds.tlsattacker.core.record.cipher.RecordCipherFactory;
+import de.rub.nds.tlsattacker.core.record.cipher.cryptohelper.KeySet;
+import de.rub.nds.tlsattacker.core.record.cipher.cryptohelper.KeySetGenerator;
+import de.rub.nds.tlsattacker.core.record.crypto.RecordDecryptor;
+import de.rub.nds.tlsattacker.core.state.TlsContext;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
 import de.rub.nds.tlsattacker.core.workflow.action.ReceiveAction;
@@ -23,13 +37,16 @@ import de.rub.nds.tlsattacker.core.workflow.action.SendingAction;
 import de.rub.nds.tlsattacker.core.workflow.action.TlsAction;
 import de.rub.nds.tlsattacker.transport.socket.SocketState;
 import de.rub.nds.tlstest.framework.constants.AssertMsgs;
+import de.rub.nds.tlstest.framework.constants.TestEndpointType;
 import de.rub.nds.tlstest.framework.constants.TestResult;
 import de.rub.nds.tlstest.framework.execution.AnnotatedState;
 import de.rub.nds.tlstest.framework.model.derivationParameter.TcpFragmentationDerivation;
+import java.math.BigInteger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -169,17 +186,20 @@ public class Validator {
             if (receivedMessages == null) {
                 receivedMessages = new ArrayList<>();
             }
-
+                
             ProtocolMessage lastExpected = expectedMessages.get(expectedMessages.size() - 1);
             if (lastExpected.getClass().equals(AlertMessage.class)) {
                 if (receivedMessages.size() > 0) {
                     ProtocolMessage lastReceivedMessage = receivedMessages.get(receivedMessages.size() - 1);
+                    AbstractRecord lastReceivedRecord = action.getReceivedRecords().get(action.getReceivedRecords().size() - 1);
                     if (lastReceivedMessage.getClass().equals(AlertMessage.class)) {
                         boolean lastMessageIsFatalAlert =
                                 AlertLevel.FATAL == AlertLevel.getAlertLevel(((AlertMessage)lastReceivedMessage).getLevel().getValue());
-                        if (lastMessageIsFatalAlert || onlyValidAlertsAfterFatalAlert(action)) {
+                        if (lastMessageIsFatalAlert || onlyValidAlertsAfterFatalAlert(action.getReceivedMessages())) {
                             return;
                         }
+                    } else if(lastMessagesAreTooEarlyEncryptedAlertsTls13(state, action)) {
+                        return;
                     }
                 }
 
@@ -217,9 +237,7 @@ public class Validator {
      * be reviewed (we set an additional result info for this in
      * receivedFatalAlert).
      */
-    private static boolean onlyValidAlertsAfterFatalAlert(ReceiveAction lastReceive) {
-        List<ProtocolMessage> receivedMessages = lastReceive.getReceivedMessages();
-        
+    private static boolean onlyValidAlertsAfterFatalAlert(List<ProtocolMessage> receivedMessages) {
         //upon calling this function, we already know there is at least 1
         //alert at the end
         if(receivedMessages.size() < 2) {
@@ -244,6 +262,77 @@ public class Validator {
         }
         
         return foundFatal;
+    }
+    
+    /**
+     * Some implementations switch to Handshake Traffic secrets too early, we thus
+     * haven't set up the decryption in the record layer yet. We attempt to decrypt
+     * records with Handshake Secrets to uncover these Alerts. We apply the same
+     * tolerance as for TLS 1.2 - the received messages must either end with a
+     * Fatal Alert of it's own or may only consist of Alerts of valid severity after
+     * receiving the first Fatal Alert (again to allow a Close Notify mostly)
+     */
+    private static boolean lastMessagesAreTooEarlyEncryptedAlertsTls13(AnnotatedState state, ReceiveAction lastReceiveAction) {
+        ProtocolMessage lastReceivedMessage = lastReceiveAction.getReceivedMessages().get(lastReceiveAction.getReceivedMessages().size() - 1);
+        List<ProtocolMessage> receivedMessages = lastReceiveAction.getReceivedMessages();
+        List<AbstractRecord> receivedRecords = lastReceiveAction.getReceivedRecords();
+        Record lastReceivedRecord = (Record) lastReceiveAction.getReceivedRecords().get(receivedRecords.size() - 1);
+        int expectedFirstEncryptedRecordIndex = 0;
+        if(receivedMessages.get(0) instanceof ChangeCipherSpecMessage) {
+            expectedFirstEncryptedRecordIndex = 1;
+        }
+        if(state.getState().getConfig().getHighestProtocolVersion() == ProtocolVersion.TLS13
+                && TestContext.getInstance().getConfig().getTestEndpointMode() == TestEndpointType.CLIENT
+                && state.getState().getTlsContext().getActiveClientKeySetType() == Tls13KeySetType.NONE
+                && lastReceivedMessage instanceof ApplicationMessage) {
+            state.addAdditionalResultInfo("Received Application Message before decryption was set");
+            
+            List<ProtocolMessage> decryptedAlerts = new LinkedList<>();
+            AlertMessage potentialAlert = null;
+            
+            for(int i = expectedFirstEncryptedRecordIndex; i < receivedRecords.size(); i++) {
+                Record recordToDecrypt = ((Record)receivedRecords.get(i));
+                recordToDecrypt.setSequenceNumber(Modifiable.explicit(BigInteger.valueOf(i - expectedFirstEncryptedRecordIndex)));
+                potentialAlert = tryToDecryptRecordWithHandshakeSecrets(state.getState().getTlsContext(),lastReceivedRecord);
+                
+                if(potentialAlert != null) {
+                    state.addAdditionalResultInfo("Client encrypted Alert too early");
+                    decryptedAlerts.add(potentialAlert);
+                } else if(decryptedAlerts.size() > 0) {
+                    //chain of Alerts was interrupted by other message type
+                    //or decryption failed
+                    state.addAdditionalResultInfo("Not all Application Messages were Alerts");
+                    return false;
+                }
+                
+                if(potentialAlert != null && potentialAlert.getLevel().getValue() == AlertLevel.FATAL.getValue()) {
+                    //last is Fatal Alert
+                    return true;
+                } else if(!decryptedAlerts.isEmpty()) {
+                    //also allow additional (Warning) Alerts if they follow a Fatal Alert
+                    return onlyValidAlertsAfterFatalAlert(decryptedAlerts);
+                }
+            }  
+        }
+        
+        return false;
+    }
+    
+    private static AlertMessage tryToDecryptRecordWithHandshakeSecrets(TlsContext context, AbstractRecord record) {
+        try {
+            KeySet keySet = KeySetGenerator.generateKeySet(context, ProtocolVersion.TLS13, Tls13KeySetType.HANDSHAKE_TRAFFIC_SECRETS);
+            RecordCipher recordCipher = RecordCipherFactory.getRecordCipher(context, keySet, context.getSelectedCipherSuite());
+            RecordDecryptor dec = new RecordDecryptor(recordCipher, context);
+            dec.decrypt(record);
+            if(record.getContentMessageType() == ProtocolMessageType.ALERT) {
+                AlertHandler handler = new AlertHandler(context);
+                AlertMessage alert = (AlertMessage) handler.parseMessage(record.getCleanProtocolMessageBytes().getValue(), 0, true).getMessage();
+                return alert;
+            }
+            return null;
+        } catch (Exception ex) {
+            return null;
+        }
     }
 
     private static boolean traceFailedBeforeAlertAction(WorkflowTrace workflowTrace) {

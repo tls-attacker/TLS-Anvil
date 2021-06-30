eecc532a949c5e0587f1e12666f6ad86bdfe3107
Fabian Albert <Fabian.Albert@rub.de>
2021-06-30 14:04:37 +0200

    Changed MaxFragmentLengthDerivation's parameter type

--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/model/derivationParameter/MaxFragmentLengthDerivation.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/model/derivationParameter/MaxFragmentLengthDerivation.java
@@ -9,35 +9,27 @@
  */
 package de.rub.nds.tlstest.framework.model.derivationParameter;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.MaxFragmentLength;
 import de.rub.nds.tlstest.framework.TestContext;
 import de.rub.nds.tlstest.framework.model.DerivationScope;
 import de.rub.nds.tlstest.framework.model.DerivationType;
 
-import java.util.AbstractMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Derivation for the max_fragment_length extension
  *
- * The parameter type is a pair of [Boolean, MaxFragmentLength].
- * If the first value is true the extension is included with the fragment length of the second value.
- * If the first value is false the extension is not included. The second value is ignored in this case.
+ * If the parameter is null no extension is included
  */
-public class MaxFragmentLengthDerivation extends DerivationParameter<Map.Entry<Boolean, MaxFragmentLength>> {
+public class MaxFragmentLengthDerivation extends DerivationParameter<MaxFragmentLength> {
 
     public MaxFragmentLengthDerivation() {
-        super(DerivationType.MAX_FRAGMENT_LENGTH, (Class<Map.Entry<Boolean, MaxFragmentLength>>)
-                ((Map.Entry<Boolean, MaxFragmentLength>)
-                (new AbstractMap.SimpleImmutableEntry<>(false, MaxFragmentLength.TWO_9)))
-                .getClass());
+        super(DerivationType.MAX_FRAGMENT_LENGTH, MaxFragmentLength.class);
     }
 
-    public MaxFragmentLengthDerivation(Map.Entry<Boolean, MaxFragmentLength> selectedValue) {
+    public MaxFragmentLengthDerivation(MaxFragmentLength selectedValue) {
         this();
         setSelectedValue(selectedValue);
     }
@@ -46,46 +38,25 @@ public class MaxFragmentLengthDerivation extends DerivationParameter<Map.Entry<B
         List<DerivationParameter> parameterValues = new LinkedList<>();
 
         for(MaxFragmentLength maxFragmentLength : MaxFragmentLength.values()){
-            Map.Entry<Boolean, MaxFragmentLength> pair = new AbstractMap.SimpleImmutableEntry<Boolean, MaxFragmentLength>(true, maxFragmentLength);
-            parameterValues.add(new MaxFragmentLengthDerivation(pair));
+            parameterValues.add(new MaxFragmentLengthDerivation(maxFragmentLength));
         }
 
         // A parameter where no max fragment length extension is included
-        Map.Entry<Boolean, MaxFragmentLength> pair = new AbstractMap.SimpleImmutableEntry<Boolean, MaxFragmentLength>(false, MaxFragmentLength.TWO_9);
-        parameterValues.add(new MaxFragmentLengthDerivation(pair));
+        parameterValues.add(new MaxFragmentLengthDerivation(null));
 
         return parameterValues;
     }
 
     @Override
     public void applyToConfig(Config config, TestContext context) {
-        Map.Entry<Boolean, MaxFragmentLength> selectedValue = getSelectedValue();
-        boolean includeAddMaxFragmentExtension = selectedValue.getKey();
-        MaxFragmentLength maxFragmentLength = selectedValue.getValue();
+        MaxFragmentLength selectedValue = getSelectedValue();
 
-        if (includeAddMaxFragmentExtension) {
-            config.setMaxFragmentLength(maxFragmentLength);
+        if (selectedValue != null) {
+            config.setMaxFragmentLength(selectedValue);
             config.setAddMaxFragmentLengthExtension(true);
         }
-        else {
-            config.setAddMaxFragmentLengthExtension(false);
-        }
     }
 
-    @Override
-    @JsonValue
-    public String jsonValue() {
-        Map.Entry<Boolean, MaxFragmentLength> selectedValue = getSelectedValue();
-        boolean includeAddMaxFragmentExtension = selectedValue.getKey();
-        MaxFragmentLength maxFragmentLength = selectedValue.getValue();
-
-        if(includeAddMaxFragmentExtension){
-            return "" + maxFragmentLength;
-        }
-        else{
-            return "EXTENSION_NOT_INCLUDED";
-        }
-    }
 
 }
 

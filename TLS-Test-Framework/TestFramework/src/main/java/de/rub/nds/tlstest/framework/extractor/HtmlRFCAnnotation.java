6e8e64dafb7274a6e81e69588409cd5e23bcf864
mmaehren <32199075+mmaehren@users.noreply.github.com>
2022-01-14 13:42:42 +0100

    Refactored to enum

--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/extractor/HtmlRFCAnnotation.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/extractor/HtmlRFCAnnotation.java
@@ -9,34 +9,33 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class HtmlRFCAnnotation {
-
+public enum HtmlRFCAnnotation {
+    MUST("red", null),
+    MUST_NOT("red", null),
+    COVERED("green", null),
+    IMPLICIT("blue", "implicit"),
+    OUT_OF_SCOPE("blue", "out_of_scope"),
+    PROTOCOL_EXTENSION("blue", "protocol_extensions"),
+    BLACKBOX_UNTESTABLE("blue", "untestable"),
+    DEPRECATED("blue", "deprecated"),
+    CONTRADICTORY("blue", "contradictory");
+    
     private static final Logger LOGGER = LogManager.getLogger();
     
-    public static final String COLOR_COVERED = "green";
-    public static final String COLOR_DEPRECATED = "blue";
-    public static final String COLOR_OUT_OF_SCOPE = "orange";
-    public static final String COLOR_IMPLICIT = "pink";
-    public static final String COLOR_PROTOCOL_EXTENSION = "gray";
-    public static final String COLOR_BLACKBOX_UNTESTABLE = "cyan";
-    public static final String COLOR_CONTRADICTORY = "white";
-    public static final String COLOR_MUST = "red";
+    private final String color;
+    private final String directory;
+    private HtmlRFCAnnotation(String color, String directory) {
+        this.color = color;
+        this.directory = directory;
+    }
 
-    public static final String DIRECTORY_DEPRECATED = "deprecated";
-    public static final String DIRECTORY_OUT_OF_SCOPE = "out_of_scope";
-    public static final String DIRECTORY_IMPLICIT = "implicit";
-    public static final String DIRECTORY_PROTOCOL_EXTENSION = "protocol_extensions";
-    public static final String DIRECTORY_BLACKBOX_UNTESTABLE = "untestable";
-    public static final String DIRECTORY_CONTRADICTORY = "contradictory";
-
-    public static LinkedHashMap<String, List<String>> getAnnotations(int rfcNumber, String path) {
-        LinkedHashMap<String, List<String>> annotationMap = new LinkedHashMap<>();
-        annotationMap.put(DIRECTORY_DEPRECATED, getAnnotations(path + DIRECTORY_DEPRECATED, rfcNumber));
-        annotationMap.put(DIRECTORY_OUT_OF_SCOPE, getAnnotations(path + DIRECTORY_OUT_OF_SCOPE, rfcNumber));
-        annotationMap.put(DIRECTORY_IMPLICIT, getAnnotations(path + DIRECTORY_IMPLICIT, rfcNumber));
-        annotationMap.put(DIRECTORY_PROTOCOL_EXTENSION, getAnnotations(path + DIRECTORY_PROTOCOL_EXTENSION, rfcNumber));
-        annotationMap.put(DIRECTORY_BLACKBOX_UNTESTABLE, getAnnotations(path + DIRECTORY_BLACKBOX_UNTESTABLE, rfcNumber));
-        annotationMap.put(DIRECTORY_CONTRADICTORY, getAnnotations(path + DIRECTORY_CONTRADICTORY, rfcNumber));
+    public static LinkedHashMap<HtmlRFCAnnotation, List<String>> getAnnotations(int rfcNumber, String path) {
+        LinkedHashMap<HtmlRFCAnnotation, List<String>> annotationMap = new LinkedHashMap<>();
+        for(HtmlRFCAnnotation annotationType : HtmlRFCAnnotation.values()) {
+            if(annotationType.isPredefinedAnnotation()) {
+                annotationMap.put(annotationType, getAnnotations(path + annotationType.getDirectory(), rfcNumber));
+            }
+        }
         return annotationMap;
     }
 
@@ -53,23 +52,16 @@ public class HtmlRFCAnnotation {
         }
         return annotatedPassages;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
     
-    public static String getColorForDirectory(String directory) {
-        switch(directory) {
-            case DIRECTORY_DEPRECATED:
-                return COLOR_DEPRECATED;
-            case DIRECTORY_BLACKBOX_UNTESTABLE:
-                return COLOR_BLACKBOX_UNTESTABLE;
-            case DIRECTORY_IMPLICIT:
-                return COLOR_IMPLICIT;
-            case DIRECTORY_OUT_OF_SCOPE:
-                return COLOR_OUT_OF_SCOPE;
-            case DIRECTORY_PROTOCOL_EXTENSION:
-                return COLOR_PROTOCOL_EXTENSION;
-            case DIRECTORY_CONTRADICTORY:
-                return COLOR_CONTRADICTORY;
-            default:
-                throw new IllegalArgumentException(directory + " is not a known annotation identifier");
-        }
+    public boolean isPredefinedAnnotation() {
+        return !(this == MUST || this == MUST_NOT || this == COVERED);
     }
 }

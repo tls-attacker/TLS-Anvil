3e1e22748fa353a31f16a9a2fd88e5841b32b76f
mmaehren <32199075+mmaehren@users.noreply.github.com>
2022-06-08 23:39:44 +0200

    Adjusted license headers

--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/coffee4j/reporter/TlsTestsuiteReporter.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/coffee4j/reporter/TlsTestsuiteReporter.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package de.rub.nds.tlstest.framework.coffee4j.reporter;
 
 import de.rub.nds.tlstest.framework.execution.AnnotatedStateContainer;
--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/junitExtensions/ExtensionContextResolver.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/junitExtensions/ExtensionContextResolver.java
@@ -1,3 +1,11 @@
+/**
+ * TLS-Test-Framework - A framework for modeling TLS tests
+ *
+ * Copyright 2022 Ruhr University Bochum
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates

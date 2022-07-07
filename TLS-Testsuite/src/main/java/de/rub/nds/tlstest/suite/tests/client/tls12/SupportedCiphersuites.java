7a16f689e4bae23889e5a711728d4bfff617b284
Fabian Albert <Fabian.Albert@rub.de>
2022-07-07 13:30:57 +0200

    fixed supportedCiphersuites with config options

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls12/SupportedCiphersuites.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/client/tls12/SupportedCiphersuites.java
@@ -9,25 +9,37 @@
  */
 package de.rub.nds.tlstest.suite.tests.client.tls12;
 
-import de.rub.nds.tlsattacker.core.constants.CipherSuite;
-import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
-import de.rub.nds.tlstest.framework.annotations.ClientTest;
-import de.rub.nds.tlstest.framework.annotations.TestDescription;
-import de.rub.nds.tlstest.framework.annotations.categories.ComplianceCategory;
-import de.rub.nds.tlstest.framework.annotations.categories.HandshakeCategory;
-import de.rub.nds.tlstest.framework.annotations.categories.InteroperabilityCategory;
-import de.rub.nds.tlstest.framework.annotations.categories.SecurityCategory;
-import de.rub.nds.tlstest.framework.constants.SeverityLevel;
-import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
-import org.junit.jupiter.api.Tag;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
+import de.rub.nds.tlstest.framework.TestSiteReport;
+import de.rub.nds.tlstest.framework.annotations.ClientTest;
+import de.rub.nds.tlstest.framework.annotations.MethodCondition;
+import de.rub.nds.tlstest.framework.annotations.ScopeExtensions;
+import de.rub.nds.tlstest.framework.annotations.TestDescription;
+import de.rub.nds.tlstest.framework.annotations.TlsTest;
+import de.rub.nds.tlstest.framework.annotations.categories.ComplianceCategory;
+import de.rub.nds.tlstest.framework.annotations.categories.HandshakeCategory;
+import de.rub.nds.tlstest.framework.annotations.categories.InteroperabilityCategory;
+import de.rub.nds.tlstest.framework.annotations.categories.SecurityCategory;
+import de.rub.nds.tlstest.framework.coffee4j.model.ModelFromScope;
+import de.rub.nds.tlstest.framework.constants.SeverityLevel;
+import de.rub.nds.tlstest.framework.execution.WorkflowRunner;
+import de.rub.nds.tlstest.framework.model.ModelType;
+import de.rub.nds.tlstest.framework.model.ParameterExtensionManager;
+import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
+
+@Tag("debug")
 @ClientTest
 public class SupportedCiphersuites extends Tls12Test {
 
@@ -38,9 +50,10 @@ public class SupportedCiphersuites extends Tls12Test {
     @HandshakeCategory(SeverityLevel.MEDIUM)
     @ComplianceCategory(SeverityLevel.HIGH)
     public void supportsMoreCiphersuitesThanAdvertised() {
-        ClientHelloMessage clientHello = context.getReceivedClientHelloMessage();
+        ClientHelloMessage clientHello = context.getSiteReport().getReceivedClientHello();
 
         List<CipherSuite> advertised = CipherSuite.getCipherSuites(clientHello.getCipherSuites().getValue());
+
         List<CipherSuite> supported = new ArrayList<>(context.getSiteReport().getCipherSuites());
         supported.addAll(context.getSiteReport().getSupportedTls13CipherSuites());
 
@@ -61,7 +74,7 @@ public class SupportedCiphersuites extends Tls12Test {
     @HandshakeCategory(SeverityLevel.MEDIUM)
     @ComplianceCategory(SeverityLevel.CRITICAL)
     public void supportsLessCiphersuitesThanAdvertised() {
-        ClientHelloMessage clientHello = context.getReceivedClientHelloMessage();
+        ClientHelloMessage clientHello = context.getSiteReport().getReceivedClientHello();
 
         List<CipherSuite> advertised = CipherSuite.getCipherSuites(clientHello.getCipherSuites().getValue());
         advertised.remove(CipherSuite.TLS_FALLBACK_SCSV);
@@ -69,6 +82,66 @@ public class SupportedCiphersuites extends Tls12Test {
         
         List<CipherSuite> supported = new ArrayList<>(context.getSiteReport().getCipherSuites());
         supported.addAll(context.getSiteReport().getSupportedTls13CipherSuites());
+
+        supported.forEach(advertised::remove);
+        advertised = advertised.stream().filter(cipherSuite -> CipherSuite.getImplemented().contains(cipherSuite) && !cipherSuite.isGOST()).collect(Collectors.toList());
+
+        assertEquals("Client supports less ciphersuites than advertised. " +
+                        advertised.parallelStream().map(Enum::name).collect(Collectors.joining(",")),
+                0,
+                advertised.size());
+    }
+
+    // === Configuration Options tests only ===
+
+    public ConditionEvaluationResult configurationOptionsTested() {
+        if (ParameterExtensionManager.getInstance().getLoadedExtensions().contains("ConfigurationOptionsExtension")) {
+            return ConditionEvaluationResult.enabled("");
+        } else {
+            return ConditionEvaluationResult.disabled("Configuration options are not tested.");
+        }
+    }
+
+    @TlsTest(description = "Evaluate if the client accepts more cipher suites than advertised. (Tested for all builds for configuration option tests)")
+    @MethodCondition(method = "configurationOptionsTested")
+    @ModelFromScope(baseModel = ModelType.EMPTY)
+    @ScopeExtensions("ConfigOptionDerivationType.ConfigurationOptionCompoundParameter")
+    public void supportsMoreCiphersuitesThanAdvertisedWithConfigOptions(ArgumentsAccessor argumentAccessor, WorkflowRunner runner) {
+        getPreparedConfig(argumentAccessor, runner);
+        TestSiteReport report = this.derivationContainer.getAssociatedSiteReport();
+
+        ClientHelloMessage clientHello = report.getReceivedClientHello();
+
+        List<CipherSuite> advertised = CipherSuite.getCipherSuites(clientHello.getCipherSuites().getValue());
+
+        List<CipherSuite> supported = new ArrayList<>(report.getCipherSuites());
+        supported.addAll(report.getSupportedTls13CipherSuites());
+
+        advertised.forEach(supported::remove);
+
+        assertEquals("Client supports more cipher suites than advertised. " +
+                        supported.parallelStream().map(Enum::name).collect(Collectors.joining(",")),
+                0,
+                supported.size());
+    }
+
+    @TlsTest(description = "Client exploration detected less supported ciphersuites than " +
+        "advertised by the client in the ClientHello message. (Tested for all builds for configuration option tests)")
+    @MethodCondition(method = "configurationOptionsTested")
+    @ModelFromScope(baseModel = ModelType.EMPTY)
+    @ScopeExtensions("ConfigOptionDerivationType.ConfigurationOptionCompoundParameter")
+    public void supportsLessCiphersuitesThanAdvertisedWithConfigOptions(ArgumentsAccessor argumentAccessor, WorkflowRunner runner) {
+        getPreparedConfig(argumentAccessor, runner);
+        TestSiteReport report = this.derivationContainer.getAssociatedSiteReport();
+
+        ClientHelloMessage clientHello = report.getReceivedClientHello();
+
+        List<CipherSuite> advertised = CipherSuite.getCipherSuites(clientHello.getCipherSuites().getValue());
+        advertised.remove(CipherSuite.TLS_FALLBACK_SCSV);
+        advertised.remove(CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV);
+        
+        List<CipherSuite> supported = new ArrayList<>(report.getCipherSuites());
+        supported.addAll(report.getSupportedTls13CipherSuites());
 
         supported.forEach(advertised::remove);
         advertised = advertised.stream().filter(cipherSuite -> CipherSuite.getImplemented().contains(cipherSuite) && !cipherSuite.isGOST()).collect(Collectors.toList());

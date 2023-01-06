8f313fdae01e07afdb477cbd1da383e29336daed
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-01-06 12:07:51 +0100

    Activated MaxFragmentLengthDerivation again

--- TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/model/derivationParameter/MaxFragmentLengthDerivation.java
+++ TLS-Test-Framework/TestFramework/src/main/java/de/rub/nds/tlstest/framework/model/derivationParameter/MaxFragmentLengthDerivation.java
@@ -1,10 +1,9 @@
 /**
  * TLS-Test-Framework - A framework for modeling TLS tests
  *
- * Copyright 2022 Ruhr University Bochum
+ * <p>Copyright 2022 Ruhr University Bochum
  *
- * Licensed under Apache License 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  */
 package de.rub.nds.tlstest.framework.model.derivationParameter;
 
@@ -13,14 +12,13 @@ import de.rub.nds.tlsattacker.core.constants.MaxFragmentLength;
 import de.rub.nds.tlstest.framework.TestContext;
 import de.rub.nds.tlstest.framework.model.DerivationScope;
 import de.rub.nds.tlstest.framework.model.DerivationType;
-
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Derivation for the max_fragment_length extension
  *
- * If the parameter is null no extension is included
+ * <p>If the parameter is null no extension is included
  */
 public class MaxFragmentLengthDerivation extends DerivationParameter<MaxFragmentLength> {
 
@@ -33,13 +31,14 @@ public class MaxFragmentLengthDerivation extends DerivationParameter<MaxFragment
         setSelectedValue(selectedValue);
     }
 
-    public List<DerivationParameter> getParameterValues(TestContext context, DerivationScope scope) {
+    public List<DerivationParameter> getParameterValues(
+            TestContext context, DerivationScope scope) {
         List<DerivationParameter> parameterValues = new LinkedList<>();
 
         // TODO The layer system fails to process messages when max length was set
-        /*for(MaxFragmentLength maxFragmentLength : MaxFragmentLength.values()){
+        for (MaxFragmentLength maxFragmentLength : MaxFragmentLength.values()) {
             parameterValues.add(new MaxFragmentLengthDerivation(maxFragmentLength));
-        }*/
+        }
 
         // A parameter where no max fragment length extension is included
         parameterValues.add(new MaxFragmentLengthDerivation(null));
@@ -56,8 +55,4 @@ public class MaxFragmentLengthDerivation extends DerivationParameter<MaxFragment
             config.setAddMaxFragmentLengthExtension(true);
         }
     }
-
-
 }
-
-

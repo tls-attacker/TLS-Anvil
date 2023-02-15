5c48ce0a8e4f6aa81cf2341c271434b7981e99f1
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-02-15 11:56:06 +0100

    Fixed selection of KeySpec type for RSA_* algorithms

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/util/SignatureValidation.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/util/SignatureValidation.java
@@ -1,13 +1,13 @@
 /**
  * TLS-Testsuite - A testsuite for the TLS protocol
  *
- * Copyright 2022 Ruhr University Bochum
+ * <p>Copyright 2022 Ruhr University Bochum
  *
- * Licensed under Apache License 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  */
 package de.rub.nds.tlstest.suite.util;
 
+import de.rub.nds.tlsattacker.core.constants.CertificateKeyType;
 import de.rub.nds.tlsattacker.core.constants.SignatureAlgorithm;
 import de.rub.nds.tlsattacker.core.constants.SignatureAndHashAlgorithm;
 import de.rub.nds.tlsattacker.core.state.State;
@@ -24,25 +24,46 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 
 public class SignatureValidation {
-    public static Boolean validationSuccessful(SignatureAndHashAlgorithm selectedSignatureAndHashAlgo, AnnotatedState annotatedState, byte[] completeSignedData, byte[] givenSignature) throws SignatureException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, IOException, InvalidKeySpecException {
+    public static Boolean validationSuccessful(
+            SignatureAndHashAlgorithm selectedSignatureAndHashAlgo,
+            AnnotatedState annotatedState,
+            byte[] completeSignedData,
+            byte[] givenSignature)
+            throws SignatureException, InvalidAlgorithmParameterException, NoSuchAlgorithmException,
+                    InvalidKeyException, IOException, InvalidKeySpecException {
         State sessionState = annotatedState.getState();
-        Signature signatureInstance = Signature.getInstance(selectedSignatureAndHashAlgo.getJavaName());
+        Signature signatureInstance =
+                Signature.getInstance(selectedSignatureAndHashAlgo.getJavaName());
         selectedSignatureAndHashAlgo.setupSignature(signatureInstance);
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(sessionState.getTlsContext().getServerCertificate().getCertificateAt(0).getSubjectPublicKeyInfo().getEncoded());
-        
+        X509EncodedKeySpec keySpec =
+                new X509EncodedKeySpec(
+                        sessionState
+                                .getTlsContext()
+                                .getServerCertificate()
+                                .getCertificateAt(0)
+                                .getSubjectPublicKeyInfo()
+                                .getEncoded());
+
         KeyFactory keyFactory;
         PublicKey publicKey;
-        if(selectedSignatureAndHashAlgo.getSignatureAlgorithm() == SignatureAlgorithm.ECDSA) {
+        if (selectedSignatureAndHashAlgo.getSignatureAlgorithm() == SignatureAlgorithm.ECDSA) {
             keyFactory = KeyFactory.getInstance("EC");
             publicKey = keyFactory.generatePublic(keySpec);
-        } else if(selectedSignatureAndHashAlgo.getSignatureAlgorithm() == SignatureAlgorithm.RSA) {
+        } else if (selectedSignatureAndHashAlgo
+                        .getSignatureAlgorithm()
+                        .getRequiredCertificateKeyType()
+                == CertificateKeyType.RSA) {
             keyFactory = KeyFactory.getInstance("RSA");
             publicKey = keyFactory.generatePublic(keySpec);
-        } else {
+        } else if (selectedSignatureAndHashAlgo.getSignatureAlgorithm() == SignatureAlgorithm.DSA) {
             keyFactory = KeyFactory.getInstance("DSA");
             publicKey = keyFactory.generatePublic(keySpec);
+        } else {
+            throw new UnsupportedOperationException(
+                    "Signature verification is not implemented for algorithm "
+                            + selectedSignatureAndHashAlgo);
         }
-        
+
         signatureInstance.initVerify(publicKey);
         signatureInstance.update(completeSignedData);
         return signatureInstance.verify(givenSignature);

4cb885b89b53ef15f7e6f80d532c01f4aa37db53
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-02-15 14:37:28 +0100

    Fixed race condition in state machine test

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/server/both/statemachine/SharedStateMachineTest.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/server/both/statemachine/SharedStateMachineTest.java
@@ -1,10 +1,9 @@
 /**
  * TLS-Testsuite - A testsuite for the TLS protocol
  *
- * Copyright 2022 Ruhr University Bochum
+ * <p>Copyright 2022 Ruhr University Bochum
  *
- * Licensed under Apache License 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  */
 package de.rub.nds.tlstest.suite.tests.server.both.statemachine;
 
@@ -22,12 +21,9 @@ import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlstest.framework.Validator;
 import de.rub.nds.tlstest.framework.execution.WorkflowRunner;
 
-/**
- * Provides test and evaluation functionalities for both TLS 1.2 and 1.3
- * server state machines
- */
+/** Provides test and evaluation functionalities for both TLS 1.2 and 1.3 server state machines */
 public class SharedStateMachineTest {
-    
+
     public static void sharedBeginWithApplicationDataTest(Config config, WorkflowRunner runner) {
         runner.setPreparedConfig(config);
         config.setDefaultApplicationMessageData("Test");
@@ -37,15 +33,15 @@ public class SharedStateMachineTest {
         workflowTrace.addTlsAction(new ReceiveAction(new AlertMessage()));
         runner.execute(workflowTrace, config).validateFinal(Validator::receivedFatalAlert);
     }
-    
+
     public static void sharedBeginWithChangeCipherSpecTest(Config config, WorkflowRunner runner) {
-       runner.setPreparedConfig(config);
-       WorkflowTrace workflowTrace = new WorkflowTrace();
-       workflowTrace.addTlsAction(new SendAction(new ChangeCipherSpecMessage()));
-       workflowTrace.addTlsAction(new ReceiveAction(new AlertMessage()));
-       runner.execute(workflowTrace, config).validateFinal(Validator::receivedFatalAlert); 
+        runner.setPreparedConfig(config);
+        WorkflowTrace workflowTrace = new WorkflowTrace();
+        workflowTrace.addTlsAction(new SendAction(new ChangeCipherSpecMessage()));
+        workflowTrace.addTlsAction(new ReceiveAction(new AlertMessage()));
+        runner.execute(workflowTrace, config).validateFinal(Validator::receivedFatalAlert);
     }
-    
+
     public static void sharedBeginWithFinishedTest(Config config, WorkflowRunner runner) {
         runner.setPreparedConfig(config);
         WorkflowTrace workflowTrace = new WorkflowTrace();
@@ -53,14 +49,15 @@ public class SharedStateMachineTest {
         workflowTrace.addTlsAction(new ReceiveAction(new AlertMessage()));
         runner.execute(workflowTrace, config).validateFinal(Validator::receivedFatalAlert);
     }
-    
-    public static void sharedSecondClientHelloAfterServerHelloTest(Config config, WorkflowRunner runner) {
+
+    public static void sharedSecondClientHelloAfterServerHelloTest(
+            Config config, WorkflowRunner runner) {
         WorkflowTrace workflowTrace = runner.generateWorkflowTrace(WorkflowTraceType.HELLO);
         workflowTrace.addTlsAction(new SendAction(new ClientHelloMessage(config)));
         workflowTrace.addTlsAction(new ReceiveAction(new AlertMessage()));
         runner.execute(workflowTrace, config).validateFinal(Validator::receivedFatalAlert);
     }
-    
+
     public static void sharedSecondClientHelloTest(Config config, WorkflowRunner runner) {
         WorkflowTrace workflowTrace = runner.generateWorkflowTrace(WorkflowTraceType.HELLO);
         ClientHelloMessage additionalClientHello = new ClientHelloMessage(config);
@@ -69,6 +66,11 @@ public class SharedStateMachineTest {
         SendAction initialSendAction = (SendAction) workflowTrace.getFirstSendingAction();
         initialSendAction.getMessages().add(additionalClientHello);
         workflowTrace.addTlsAction(new ReceiveAction(new AlertMessage()));
-        runner.execute(workflowTrace, config).validateFinal(Validator::receivedFatalAlert);
+        // only check for alert + closed but not asExpected() since there
+        // may be a race condition based on when the server processes the
+        // 2nd client hello
+        runner.execute(workflowTrace, config)
+                .validateFinal(
+                        annotatedState -> Validator.receivedFatalAlert(annotatedState, false));
     }
 }

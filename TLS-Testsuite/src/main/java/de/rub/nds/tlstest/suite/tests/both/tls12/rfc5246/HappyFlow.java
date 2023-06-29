c1d5634c709fde0ac23ba54c3234fc902e3246d4
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-06-29 17:03:53 +0200

    Code cleanup

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/both/tls12/rfc5246/HappyFlow.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/both/tls12/rfc5246/HappyFlow.java
@@ -1,10 +1,9 @@
 /**
  * TLS-Testsuite - A testsuite for the TLS protocol
  *
- * Copyright 2022 Ruhr University Bochum
+ * <p>Copyright 2022 Ruhr University Bochum
  *
- * Licensed under Apache License 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  */
 package de.rub.nds.tlstest.suite.tests.both.tls12.rfc5246;
 
@@ -30,16 +29,15 @@ import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
-/**
- * Runs a benign handshake with all default derivations to identify
- * parameter-related bugs.
- */
+/** Runs a benign handshake with all default derivations to identify parameter-related bugs. */
 @Tag("happyflow12")
 @RFC(number = 5246, section = "7.3.  Handshake Protocol Overview")
 public class HappyFlow extends Tls12Test {
 
-    @TlsTest(description = "A benign handshake executed with all parameter combinations "
-            + " that affect a regular handshake")
+    @TlsTest(
+            description =
+                    "A benign handshake executed with all parameter combinations "
+                            + " that affect a regular handshake")
     @ModelFromScope(baseModel = ModelType.CERTIFICATE)
     @HandshakeCategory(SeverityLevel.HIGH)
     @InteroperabilityCategory(SeverityLevel.CRITICAL)
@@ -50,13 +48,28 @@ public class HappyFlow extends Tls12Test {
         SignatureAndHashAlgorithm selectedSigAndHash;
         CertificateKeyPair selectedCertificate;
 
-        runner.execute(workflowTrace, c).validateFinal(i -> {
-            boolean receivedAlert = WorkflowTraceUtil.didReceiveMessage(ProtocolMessageType.ALERT, i.getWorkflowTrace());
-            if(receivedAlert) {
-                AlertMessage alert = (AlertMessage)WorkflowTraceUtil.getFirstReceivedMessage(ProtocolMessageType.ALERT, i.getWorkflowTrace());
-                LOGGER.error("Received Alert " + AlertDescription.getAlertDescription(alert.getDescription().getValue()) + " for Happy Flow using derivations:\n" + derivationContainer.toString() + "\nWorkflowTrace:\n" + i.getWorkflowTrace().toString());
-            }
-            Validator.executedAsPlanned(i);
-        });
+        runner.execute(workflowTrace, c)
+                .validateFinal(
+                        i -> {
+                            boolean receivedAlert =
+                                    WorkflowTraceUtil.didReceiveMessage(
+                                            ProtocolMessageType.ALERT, i.getWorkflowTrace());
+                            if (receivedAlert) {
+                                AlertMessage alert =
+                                        (AlertMessage)
+                                                WorkflowTraceUtil.getFirstReceivedMessage(
+                                                        ProtocolMessageType.ALERT,
+                                                        i.getWorkflowTrace());
+                                LOGGER.error(
+                                        "Received Alert "
+                                                + AlertDescription.getAlertDescription(
+                                                        alert.getDescription().getValue())
+                                                + " for Happy Flow using derivations:\n"
+                                                + derivationContainer.toString()
+                                                + "\nWorkflowTrace:\n"
+                                                + i.getWorkflowTrace().toString());
+                            }
+                            Validator.executedAsPlanned(i);
+                        });
     }
 }

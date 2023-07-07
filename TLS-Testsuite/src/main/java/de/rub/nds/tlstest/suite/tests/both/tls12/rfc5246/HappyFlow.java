a8165a9315dd65d488e37090480c029cd3e51b38
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-07-07 14:41:15 +0200

    Set new ModelFromScope annotation

--- TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/both/tls12/rfc5246/HappyFlow.java
+++ TLS-Testsuite/src/main/java/de/rub/nds/tlstest/suite/tests/both/tls12/rfc5246/HappyFlow.java
@@ -8,11 +8,10 @@
 package de.rub.nds.tlstest.suite.tests.both.tls12.rfc5246;
 
 import de.rub.nds.anvilcore.annotation.AnvilTest;
-import de.rub.nds.tlsattacker.core.certificate.CertificateKeyPair;
+import de.rub.nds.anvilcore.coffee4j.model.ModelFromScope;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.AlertDescription;
 import de.rub.nds.tlsattacker.core.constants.ProtocolMessageType;
-import de.rub.nds.tlsattacker.core.constants.SignatureAndHashAlgorithm;
 import de.rub.nds.tlsattacker.core.protocol.message.AlertMessage;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
@@ -21,10 +20,8 @@ import de.rub.nds.tlstest.framework.Validator;
 import de.rub.nds.tlstest.framework.annotations.RFC;
 import de.rub.nds.tlstest.framework.annotations.categories.HandshakeCategory;
 import de.rub.nds.tlstest.framework.annotations.categories.InteroperabilityCategory;
-import de.rub.nds.tlstest.framework.coffee4j.model.ModelFromScope;
 import de.rub.nds.tlstest.framework.constants.SeverityLevel;
 import de.rub.nds.tlstest.framework.execution.WorkflowRunner;
-import de.rub.nds.tlstest.framework.model.TlsModelType;
 import de.rub.nds.tlstest.framework.testClasses.Tls12Test;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
@@ -38,15 +35,12 @@ public class HappyFlow extends Tls12Test {
             description =
                     "A benign handshake executed with all parameter combinations "
                             + " that affect a regular handshake")
-    @ModelFromScope(baseModel = TlsModelType.CERTIFICATE)
+    @ModelFromScope(modelType = "CERTIFICATE")
     @HandshakeCategory(SeverityLevel.HIGH)
     @InteroperabilityCategory(SeverityLevel.CRITICAL)
     public void happyFlow(ArgumentsAccessor argumentAccessor, WorkflowRunner runner) {
         Config c = getPreparedConfig(argumentAccessor, runner);
         WorkflowTrace workflowTrace = runner.generateWorkflowTrace(WorkflowTraceType.HANDSHAKE);
-
-        SignatureAndHashAlgorithm selectedSigAndHash;
-        CertificateKeyPair selectedCertificate;
 
         runner.execute(workflowTrace, c)
                 .validateFinal(

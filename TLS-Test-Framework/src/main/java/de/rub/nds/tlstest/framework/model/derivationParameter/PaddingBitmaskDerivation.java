92188336badcf5dd264fc204d649d8fd9650af7b
Conradowatz <Conrad.Schmidt@hackmanit.de>
2023-09-12 15:31:43 +0200

    change . to : in scope notation

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/model/derivationParameter/PaddingBitmaskDerivation.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/model/derivationParameter/PaddingBitmaskDerivation.java
@@ -177,7 +177,7 @@ public class PaddingBitmaskDerivation extends TlsDerivationParameter<Integer> {
                                 TlsParameterType.APP_MSG_LENGHT.name(),
                                 TlsParameterType.INCLUDE_ENCRYPT_THEN_MAC_EXTENSION.name(),
                                 getParameterIdentifier()
-                                        + "."
+                                        + ":"
                                         + TlsParameterType.BIT_POSITION.name())
                         .by(
                                 (PaddingBitmaskDerivation paddingBitmaskDerivation,

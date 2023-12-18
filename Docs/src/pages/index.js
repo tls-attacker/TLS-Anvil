bcd0822d3bf2f1714d0181a2b2c16b19ec92d4cc
Conradowatz <Conrad.Schmidt@hackmanit.de>
2023-12-18 14:43:45 +0100

    header meta fields

--- Docs/src/pages/index.js
+++ Docs/src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`TLS-Anvil`}
+      description="A fully automated TLS test suite for clients and servers.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

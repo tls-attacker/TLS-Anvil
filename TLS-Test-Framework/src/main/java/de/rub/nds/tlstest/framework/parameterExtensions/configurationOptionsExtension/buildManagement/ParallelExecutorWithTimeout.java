4098605a184723809545332fd6546f30d6a50ab8
Conradowatz <Conrad.Schmidt@hackmanit.de>
2025-04-10 15:33:06 +0200

    bump protocol toolkit bom to 5.1.11

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/parameterExtensions/configurationOptionsExtension/buildManagement/ParallelExecutorWithTimeout.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/parameterExtensions/configurationOptionsExtension/buildManagement/ParallelExecutorWithTimeout.java
@@ -14,8 +14,6 @@ import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
 import de.rub.nds.tlsattacker.core.workflow.task.ITask;
 import de.rub.nds.tlsattacker.core.workflow.task.TlsTask;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,40 +30,6 @@ public class ParallelExecutorWithTimeout extends ParallelExecutor {
                 new ThreadPoolExecutor(
                         size, size, 10L, TimeUnit.DAYS, new LinkedBlockingDeque<>()));
         this.timeoutAfter = timeoutSec;
-    }
-
-    /**
-     * Ugly way to access the private addStateTask function of the parallel executor. TODO: Request
-     * to make TLS-Attackers addStateTask method protected.
-     */
-    private Future<ITask> addStateTask(State state) {
-        try {
-            Method addStateTaskMethod =
-                    ParallelExecutor.class.getDeclaredMethod("addStateTask", State.class);
-            addStateTaskMethod.setAccessible(true);
-            return (Future<ITask>) addStateTaskMethod.invoke(this, state);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-            throw new UnsupportedOperationException(
-                    "addStateTask of ParallelExecutor does not exist (anymore?).");
-        }
-    }
-
-    /**
-     * Ugly way to access the private addTask function of the parallel executor. TODO: Request to
-     * make TLS-Attackers addTask method protected.
-     */
-    private Future<ITask> addTask(TlsTask task) {
-        try {
-            Method addTaskMethod =
-                    ParallelExecutor.class.getDeclaredMethod("addTask", TlsTask.class);
-            addTaskMethod.setAccessible(true);
-            return (Future<ITask>) addTaskMethod.invoke(this, task);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-            throw new UnsupportedOperationException(
-                    "addTask of ParallelExecutor does not exist (anymore?).");
-        }
     }
 
     @Override
